0ce0d41e2fe395af513db5d61130badb340f880c
Tanya Bihag <tbihag@gmail.com>
2019-09-01 01:47:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,16 @@
 class Dog 
+  def name
+    @name 
+  end
   
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def breed 
+    @breed
+  end
+  
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+end 
